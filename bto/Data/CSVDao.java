6ae0668a3512f4959c1c12b24668d6826aa2583e
J370 <23294168+J370@users.noreply.github.com>
2025-04-15 13:22:55 +0800

    CSVDao completed

--- bto/Data/CSVDao.java
+++ bto/Data/CSVDao.java
@@ -7,19 +7,27 @@ public class CSVDao {
     public void readUser(String filePath) {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
+            Applicant applicant = null;
+            boolean first = true;
             while ((line = br.readLine()) != null) {
-                // use comma as separator
-                String[] data = line.split(",");
-                switch (filePath) {
-                    case "ApplicantList.csv":
-                        Applicant.add(new Applicant(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]));
-                        break;
-                    // case "ManagerList.csv":
-                    //     Manager.add(new Manager(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]));
-                    //     break;
-                    // case "OfficerList.csv":
-                    //     Officer.add(new Officer(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]));
-                    //     break;
+                if (first) {
+                    first = false;
+                }
+                else {
+                    // use comma as separator
+                    String[] data = line.split(",");
+                    filePath = filePath.replace("./bto/Data/", "");
+                    switch (filePath) {
+                        case "ApplicantList.csv":
+                            new Applicant(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]);
+                            break;
+                        // case "ManagerList.csv":
+                        //     new Manager(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]);
+                        //     break;
+                        // case "OfficerList.csv":
+                        //     new Officer(data[0], data[1], Integer.parseInt(data[2]), data[3], data[4]);
+                        //     break;
+                    }
                 }
             }
         } catch (IOException e) {
